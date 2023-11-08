b4af4f238c97f1193e611230f535e8eb46a93843
Araya Musawwah <miramadhan354@gmail.com>
2023-11-08 22:16:40 +0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
-    const absolute = document.querySelector('.absolute');
+    const absolute = document.getElementsByClassName('absolute');
+    const toTop = document.querySelector('.to-top');
     const fixedNav = header.offsetTop;
 
     if(window.scrollY > fixedNav ) {
         header.classList.add("navbar-fixed");
         header.classList.remove('absolute');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
         header.classList.add('absolute');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 
 }
@@ -21,3 +26,17 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+const html = document.querySelector('html');
+const darkToggle = document.querySelector('.dark-toggle');
+
+darkToggle.addEventListener('click', () =>{
+    darkToggle.checked ? html.classList.add('dark') : html.classList.remove('dark');
+});
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
