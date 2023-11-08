4daf8cadf83dff9c2213a2aa8d332cfd39125cb0
Araya Musawwah <miramadhan354@gmail.com>
2023-11-08 23:25:07 +0700

    update yang ke sejutax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./dist/**/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {
       colors:{
         'primary': '#5b21b6',
         'secondary': '#d1d5db',
+        'tertiary': '#1e293b',
         'lightPrimary': 'rgb(228 228 231)',
+        'darkPrimary': 'rgb(17 24 39)',
       },
 
       fontFamily:{
